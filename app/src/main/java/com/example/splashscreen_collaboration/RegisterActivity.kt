1853bf3b23750093ab6e0936c87d67c3ff99b4bd
ST10095103 <ST10095103@vcconnect.edu.za>
2024-02-29 16:05:49 +0200

    Registration complete. Just need to see if work. Havent stored Username/email or password as yet. Also didnt link to firebase...

--- app/src/main/java/com/example/splashscreen_collaboration/RegisterActivity.kt
+++ app/src/main/java/com/example/splashscreen_collaboration/RegisterActivity.kt
@@ -1,11 +1,27 @@
 package com.example.splashscreen_collaboration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var regBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        regBtn = findViewById(R.id.RegisterBtn)
+
+        regBtn.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
+
     }
 }
