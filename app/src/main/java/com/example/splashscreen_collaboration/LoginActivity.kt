108fc60a7ec4ead0b6e1bbf60c0bea089a89ba92
Radhya Chinnapen <ST10069610@vcconnect.edu.za>
2024-03-06 09:34:51 +0200

    Finished splash screen

--- app/src/main/java/com/example/splashscreen_collaboration/LoginActivity.kt
+++ app/src/main/java/com/example/splashscreen_collaboration/LoginActivity.kt
@@ -1,11 +1,14 @@
 package com.example.splashscreen_collaboration
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
     }
 }
